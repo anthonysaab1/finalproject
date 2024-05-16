687dafc040f1a544a18bb118285254d967262716
AnthonySaab <anthonysaab231@gmail.com>
2024-05-16 12:00:17 +0300

    newFolders

---
+++ favorite.php
@@ -0,0 +1,33 @@
+<?php
+header('Content-Type: application/json');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "movies";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT * FROM user_favorite";
+$result = $conn->query($sql);
+
+$movies = array();
+
+if ($result->num_rows > 0) {
+  
+  while ($row = $result->fetch_assoc()) {
+    $movies[] = $row;
+  }
+}
+
+
+$conn->close();
+
+echo json_encode($movies);die;
+
+?>
